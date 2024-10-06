c44498f5c6cb25c7ac74f2f26336a0a1d1ec1b33
calebs524 <calebsatvedi@gmail.com>
2024-10-06 13:54:41 -0500

    server issues

--- app.cjs
+++ app.cjs
@@ -207,6 +207,6 @@ app.put('/api/updatelocation/:uid', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
